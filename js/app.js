c966663a8ac74512d37c58e8d65954561385d5c4
AWeidenkopf <oliveira.anacalado@gmail.com>
2022-09-12 20:01:30 -0400

    create initial board

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-console.log("hei")
+
+/*-------------------------------- Constants --------------------------------*/
+/*-------------------------------- Variables --------------------------------*/
+
+let board
+
+/*------------------------ Cached Element References ------------------------*/
+
+const gameContainer = document.getElementById('game-container')
+
+/*----------------------------- Event Listeners -----------------------------*/
+/*-------------------------------- Functions --------------------------------*/
+init()
+
+function init() {
+
+  for(i = 0; i <= 359; i++){
+    let eachSquare = document.createElement('div')
+    eachSquare.setAttribute('id', `sq${i}`)
+    eachSquare.setAttribute('class', 'innerSquare')
+    gameContainer.appendChild(eachSquare)
+  }
+}
