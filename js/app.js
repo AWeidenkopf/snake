a52c9353acfeec032132edc87ff9285beaf6112f
AWeidenkopf <oliveira.anacalado@gmail.com>
2022-09-13 10:59:45 -0400

    create initial snake && apple

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@
 /*-------------------------------- Constants --------------------------------*/
 /*-------------------------------- Variables --------------------------------*/
 
-let board
+let board, newSquare, square, snake, score, 
+currIdx, appleIdx, direction 
 
 /*------------------------ Cached Element References ------------------------*/
 
@@ -15,9 +16,38 @@ init()
 function init() {
 
   for(i = 0; i <= 359; i++){
-    let eachSquare = document.createElement('div')
-    eachSquare.setAttribute('id', `sq${i}`)
-    eachSquare.setAttribute('class', 'innerSquare')
-    gameContainer.appendChild(eachSquare)
+    newSquare = document.createElement('div')
+    newSquare.setAttribute('id', `sq${i}`)
+    newSquare.setAttribute('class', 'innerSquare')
+    gameContainer.appendChild(newSquare)
   }
+  square = document.querySelectorAll('div.innerSquare')
+
+  start()
 }
+
+function start(){
+
+  console.log(square)
+  snake = [0, 1, 2]
+  direction = 1;
+  currIdx = 0;
+  getSnake()
+  apple()
+}
+
+function getSnake() {
+
+  snake.forEach((el, idx) => {
+    console.log(square[el])
+    square[idx].classList.add('snake')
+  })
+}
+
+function apple() {
+  
+  let currAppleIdx = (Math.floor(Math.random() * square.length))
+
+  square[currAppleIdx].style.backgroundColor = 'red';
+}
+
